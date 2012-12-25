4df217a6f8749ae1f49c825f596ecd5238bca2c3
Hongxun Zhang <hongxun@appannie.com>
2012-12-25 11:43:54 +0800

    Use EAEP to refactor the dirlock.

--- python/dirlock.py
+++ python/dirlock.py
@@ -1,5 +1,6 @@
 import os
 import functools
+import time
 
 def transactional(func):
     lock_root = os.path.join('locks', func.__name__)
@@ -13,33 +14,41 @@ def transactional(func):
         lock_dir_started = os.path.join(lock_root, '%s-started' % date.strftime('%Y-%m-%d'))
         lock_dir_finished = os.path.join(lock_root, '%s-finished' % date.strftime('%Y-%m-%d'))
 
-        # XXX could do EAFP here to totally eliminate window for racecondition
-        if os.path.exists(lock_dir_started):
-            if os.path.exists(lock_dir_finished):
-                print 'job %s already done. skipping.' % date
-            else:
+        try:
+            os.makedirs(lock_dir_started)
+            ans = func(date)
+            os.makedirs(lock_dir_finished)
+            return ans
+        except OSError:
+            try:
+                os.makedirs(lock_dir_finished)
+                print "job %s alreday done. skipping." % date
+            except OSError:
                 print 'job %s already started or running. cleanup if you want to restart.' % date
-            return
-
-        os.makedirs(lock_dir_started)
-        ans = func(date)
-        os.makedirs(lock_dir_finished)
-        return ans
     return wrapper
+
 
 @transactional
 def test(date):
+    time.sleep(4)
     print "test done! %s" % str(date)
 
 if __name__ == "__main__":
     from datetime import datetime
     from threading import Thread
+    from datetime import timedelta
+    import random
 
-    datenow = datetime.now()
-    num = 10
+    daterange = []
+    startdate = datetime.now()
+    enddate = startdate + timedelta(days=5)
+    while startdate <= enddate:
+        daterange.append(startdate)
+        startdate += timedelta(days=1)
+    num = 20
     holder = []
     for i in range(num):
-        t = Thread(target=test, args=(datenow, ))
+        t = Thread(target=test, args=(daterange[random.randint(0, len(daterange)-1)], ))
         holder.append(t)
 
     for t in holder:

b2f840bbe56c7fe4dbc55388421e7e3423fe98a9
Hongxun Zhang <hongxun@appannie.com>
2012-12-26 11:28:42 +0800

    Add header and set option

--- shell/daterange.sh
+++ shell/daterange.sh
@@ -1,6 +1,11 @@
-date="$1"
-enddate=`date -d "+1 day $2" +%Y%m%d`
-while [[ $date < $enddate  ]]  
+#!/bin/bash
+set -e
+set -o pipefail
+startday="20120101"
+endday="20120131"
+date="$startday"
+enddate=`date -d "+1 day $endday" +%Y%m%d`
+while [[ $date < $enddate ]]
 do
     date=`date -d "+1 day $date" +%Y%m%d`
     echo $date
